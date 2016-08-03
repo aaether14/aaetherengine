e927252060dfdb1d41df652c090a1b8da6eb2911
Aaether <aaetherengine@gmail.com>
2016-08-03 09:03:50 +0300

    Better way of doing stuff!

--- aae_utils/aae_linked_list.h
+++ aae_utils/aae_linked_list.h
@@ -1,20 +1,7 @@
 /**
-*aae_linked_list.h - Dinescu Stefan-Cristian aka Aaether - 31.07.2016
-*/
-
-
-#ifndef AAE_LINKED_LIST_H
-#define AAE_LINKED_LIST_H
-
-
-
-#include <malloc.h>
-#include "aae_types.h"
-
-
-
-/**
-Code generation for linked list holding TYPE data 
+---aae_linked_list.h - Dinescu Stefan-Cristian aka Aaether - 31.07.2016
+----------------------------------------------------------------------
+Code generation for linked list holding TYPE data
 -td stands for typedef
 -don't ever use variables, functions etc. starting with "__". Only the framework has this right.
 Features:
@@ -27,101 +14,99 @@ Safe types:
 
 
 
+
+
+#ifndef AAE_LINKED_LIST_H
+#define AAE_LINKED_LIST_H
+#include "aae_types.h"
+#include <malloc.h>
+
+
 /**
 *Posible return values for the linked list
 */
 typedef enum
 	{	AAE_LINKED_LIST_ALLOCATION_ERROR = -2, //malloc error
-		AAE_LINKED_LIST_INVALID_LIST = -1, //invalid list pointer 
+		AAE_LINKED_LIST_INVALID_LIST = -1, //invalid list pointer
 		AAE_LINKED_LIST_FALSE = 0,
 		AAE_LINKED_LIST_TRUE = 1,
 		AAE_LINKED_LIST_SUCCESS = 2
 	} AAE_LINKED_LIST_RETURN_VALUES;
+#endif /* AAE_LINKED_LIST_H*/
 
 
 
-#define AAE_DEFINE_LINKED_LIST(TYPE)														\
-																		\
-																		\
-																		\
-																		\
-	typedef struct __linked_list_elem_##TYPE												\
-	{																	\
-		TYPE m_data;															\
-		__linked_list_elem_##TYPE * m_next;												\
-	} __td_linked_list_elem_##TYPE;														\
-																		\
-																		\
-																		\
-	typedef struct __linked_list_##TYPE													\
-	{																	\
-		aae_size_t m_size;														\
-		__td_linked_list_elem_##TYPE * m_first;												\
-		__td_linked_list_elem_##TYPE * m_last;												\
-		__td_linked_list_elem_##TYPE * m_it;												\
-	} __td_linked_list_##TYPE;														\
-																		\
-																		\
-																		\
-																		\
-	inline static AAE_LINKED_LIST_RETURN_VALUES __linked_list_push_back_##TYPE(__td_linked_list_##TYPE * pList, TYPE tValue)		\
-	{																	\
-		if (!pList) return AAE_LINKED_LIST_INVALID_LIST;										\
-		__td_linked_list_elem_##TYPE * pNewElem = (__td_linked_list_elem_##TYPE*)malloc(sizeof(__td_linked_list_elem_##TYPE));		\
-		if (!pNewElem) return AAE_LINKED_LIST_SUCCESS;											\
-		pNewElem->m_next = NULL; pNewElem->m_data = tValue;										\
-		if (!pList->m_first) pList->m_first = pList->m_last = pNewElem;									\
-		else { pList->m_last->m_next = pNewElem; pList->m_last = pNewElem; }								\
-		pList->m_size++;														\
-		return AAE_LINKED_LIST_SUCCESS;													\
-	}																	\
-																		\
-																		\
-																		\
-	inline static AAE_LINKED_LIST_RETURN_VALUES __linked_list_push_front_##TYPE(__td_linked_list_##TYPE * pList, TYPE tValue)		\
-	{																	\
-		if (!pList) return AAE_LINKED_LIST_INVALID_LIST;										\
-		__td_linked_list_elem_##TYPE * pNewElem = (__td_linked_list_elem_##TYPE*)malloc(sizeof(__td_linked_list_elem_##TYPE));		\
-		if (!pNewElem) return AAE_LINKED_LIST_ALLOCATION_ERROR;										\
-		pNewElem->m_next = pList->m_first; pNewElem->m_data = tValue;									\
-		if (!pList->m_first) pList->m_first = pList->m_last = pNewElem;									\
-		else { pList->m_first = pNewElem; }												\
-		pList->m_size++;														\
-		return AAE_LINKED_LIST_SUCCESS;													\
-	}																	\
-																		\
-																		\
-																		\
-	inline static __td_linked_list_##TYPE * __linked_list_new_list_##TYPE()									\
-	{																	\
-		__td_linked_list_##TYPE * pNewList = (__td_linked_list_##TYPE*)malloc(sizeof(__td_linked_list_##TYPE));				\
-		pNewList->m_first = pNewList->m_last = pNewList->m_it = NULL;									\
-		pNewList->m_size = 0;														\
-		return pNewList;														\
-	}																	\
-																		\
-																		\
-																		\
-	inline static AAE_LINKED_LIST_RETURN_VALUES __linked_list_free_nodes_##TYPE(__td_linked_list_##TYPE * pList)				\
-	{																	\
-		if (!pList) return AAE_LINKED_LIST_INVALID_LIST;										\
-		pList->m_it = pList->m_first;													\
-		while (pList->m_it != NULL)													\
-		{																\
-			__td_linked_list_elem_##TYPE * pTempElem = pList->m_it;									\
-			pList->m_it = pList->m_it->m_next;											\
-			free(pTempElem);													\
-		}																\
-		pList->m_first = pList->m_last = pList->m_it = NULL;										\
-		pList->m_size = 0;														\
-		return AAE_LINKED_LIST_SUCCESS;													\
+
+
+
+#ifndef AAE_LINKED_LIST_TYPE
+#error "Must define AAE_LINKED_LIST_TYPE!"
+#endif
+
+
+
+/**
+*Indirection is used in order to achieve defined token concatenation
+*/
+#define TOKENPASTE(x, y) x##y
+#define INDIRECT_TOKENPASTE(x, y) TOKENPASTE(x, y)
+#define __AAE_LINKED_LIST_PREFIX(NAME) INDIRECT_TOKENPASTE(INDIRECT_TOKENPASTE(__linked_list, NAME), AAE_LINKED_LIST_TYPE)
+#define __AAE_LINKED_LIST_PREFIX_TD(NAME) INDIRECT_TOKENPASTE(INDIRECT_TOKENPASTE(__td_linked_list, NAME), AAE_LINKED_LIST_TYPE)
+
+
+
+
+	typedef struct __AAE_LINKED_LIST_PREFIX(_elem)
+	{
+		AAE_LINKED_LIST_TYPE m_data;
+		__AAE_LINKED_LIST_PREFIX(_elem) * m_next;
+	} __AAE_LINKED_LIST_PREFIX_TD(_elem);
+
+
+
+	typedef struct __AAE_LINKED_LIST_PREFIX(_)
+	{
+		aae_size_t m_size;
+		__AAE_LINKED_LIST_PREFIX_TD(_elem) * m_first;
+		__AAE_LINKED_LIST_PREFIX_TD(_elem) * m_last;
+		__AAE_LINKED_LIST_PREFIX_TD(_elem) * m_it;
+	} __AAE_LINKED_LIST_PREFIX_TD(_);
+
+
+
+	AAE_LINKED_LIST_RETURN_VALUES __AAE_LINKED_LIST_PREFIX(_push_back)(__AAE_LINKED_LIST_PREFIX_TD(_) * pList, AAE_LINKED_LIST_TYPE tValue)
+	{
+
+		if (!pList) return AAE_LINKED_LIST_INVALID_LIST;
+
+
+		__AAE_LINKED_LIST_PREFIX_TD(_elem) * pNewElem = (__AAE_LINKED_LIST_PREFIX_TD(_elem)*)malloc(sizeof(__AAE_LINKED_LIST_PREFIX_TD(_elem)));
+		if (!pNewElem) return AAE_LINKED_LIST_SUCCESS;
+
+
+		pNewElem->m_next = NULL; pNewElem->m_data = tValue;
+		if (!pList->m_first) pList->m_first = pList->m_last = pNewElem;
+		else { pList->m_last->m_next = pNewElem; pList->m_last = pNewElem; }
+
+
+		pList->m_size++;
+		return AAE_LINKED_LIST_SUCCESS;
 	}
-		
-		
-		
 
 
-/** 
+
+	__AAE_LINKED_LIST_PREFIX_TD(_) * __AAE_LINKED_LIST_PREFIX(_new_list)()
+	{
+		
+		__AAE_LINKED_LIST_PREFIX_TD(_) * pNewList = (__AAE_LINKED_LIST_PREFIX_TD(_)*)malloc(sizeof(__AAE_LINKED_LIST_PREFIX_TD(_)));
+		pNewList->m_first = pNewList->m_last = pNewList->m_it = NULL;									
+		pNewList->m_size = 0;														
+		return pNewList; 	
+
+	}
+
+
+/**
 *Types
 */
 #define aae_LinkedList(TYPE)					__td_linked_list_##TYPE //Defines a linked list of a certain TYPE
@@ -138,12 +123,12 @@ m_size - the size of the list
 /**
 *Functions
 */
-#define aae_NewLinkedList(TYPE)					__linked_list_new_list_##TYPE() //will return NULL if memory allocation failed
-#define aae_LinkedListPushBack(TYPE, list, elem)		__linked_list_push_back_##TYPE(list, elem)
-#define aae_LinkedListPushFront(TYPE, list, elem)		__linked_list_push_front_##TYPE(list, elem)
-#define aae_LinkedListFreeNodes(TYPE, list)			__linked_list_free_nodes_##TYPE(list)	
+#define aae_LinkedListPushBack(TYPE, list, elem)		__linked_list_push_back##TYPE(list, elem)
+#define aae_LinkedListNewList(TYPE)				__linked_list_new_list##TYPE()
 
 
 
 
+#ifdef AAE_LINKED_LIST_TYPE
+#undef AAE_LINKED_LIST_TYPE
 #endif
--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
+
+
+#define AAE_LINKED_LIST_TYPE float
 #include "aae_utils/aae_linked_list.h"
-
-
-AAE_DEFINE_LINKED_LIST(float)
 
 
 
@@ -13,24 +14,20 @@ int main(
     )
 {
 
-
-	aae_LinkedList(float) * pList = aae_NewLinkedList(float);
-	aae_LinkedListPushBack(float, pList, 5.32); 
-	aae_LinkedListPushBack(float, pList, 3.14); 
-        aae_LinkedListPushFront(float,pList, 24521.15);
-	aae_LinkedListPushBack(float, pList, 1.72);
-	aae_LinkedListPushBack(float, pList, 213213.23);
-	aae_LinkedListPushFront(float, pList, 3.152);
+	
+	aae_LinkedList(float) * pList = aae_LinkedListNewList(float);
+	aae_LinkedListPushBack(float, pList, 2.25);
+	aae_LinkedListPushBack(float, pList, 3.14);
+	aae_LinkedListPushBack(float, pList, 1241.32);
 
 
-	for (pList->m_it = pList->m_first; pList->m_it!= NULL; pList->m_it = pList->m_it->m_next)
-	printf("%2.3f\n", pList->m_it->m_data);
-	printf("There are %lu members in the list.\n", pList->m_size);
+	for (pList->m_it = pList->m_first; pList->m_it != NULL; pList->m_it = pList->m_it->m_next)
+	printf("%2.2f\n", pList->m_it->m_data);
 
 
-	aae_LinkedListFreeNodes(float, pList);
 	free(pList);
 	pList = NULL;
+
 
  
 	return 0;
