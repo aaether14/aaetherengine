e49d3b01a1000381cb8e0f4700d8b4b795ec7447
Aaether <aaetherengine@gmail.com>
2016-08-05 18:38:20 +0300

    test commit2

--- git_push.sh
+++ git_push.sh
@@ -1,9 +1,9 @@
 #GIT PUSH MACRO by Aaether
 
 
-	printf "Adding all files in the repository!"
+	printf "Adding all files in the repository!\n"
 	git add .
-	printf "Commiting chagnes!"
+	printf "Commiting chagnes!\n"
 	git commit -m "$1"
-	printf "Pushing commit to outside source!"
+	printf "Pushing commit to outside source!\n"
 	git push
